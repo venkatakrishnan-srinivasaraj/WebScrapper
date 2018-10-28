ca4472a50b117d7ae0edf6bf45f62c429549a8c4
Venkatakrishnan Srinivasaraj <venkatakrishnan111@gmail.com>
2018-10-28 03:08:04 -0500

    Fixing the unit test
    Used the AnnotationConfigContextLoader to give the config classes

--- src/test/java/com/utdevelopers/webscrapper/WebScrapperApplicationTests.java
+++ src/test/java/com/utdevelopers/webscrapper/WebScrapperApplicationTests.java
@@ -3,10 +3,16 @@ package com.utdevelopers.webscrapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import com.utdevelopers.webscraper.config.ApartmentScrapperConfig;
+import com.utdevelopers.webscraper.config.ImporterConfig;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes= {ApartmentScrapperConfig.class,ImporterConfig.class},loader=AnnotationConfigContextLoader.class)
 public class WebScrapperApplicationTests {
 
 	@Test
